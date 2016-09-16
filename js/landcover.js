41fc86e91dce7ce7727d66cb437cf725111dd6c9
shaunakv1 <shaunakv1@gmail.com>
2016-09-16 00:27:42 -0400

    displaying CCAP_landcover_2010 layer as a raster source that can be manipulated

--- js/landcover.js
+++ js/landcover.js
@@ -1,3 +1,27 @@
+/**
+ * Setup landcover tile cache layer as a raster layer
+ */
+var landcover = new ol.source.Raster({
+  sources: [
+    new ol.source.XYZ({
+      url: 'https://coast.noaa.gov/arcgis/rest/services/CCAP/CCAP_landcover_2010/MapServer/tile/{z}/{y}/{x}',
+      crossOrigin: 'anonymous'
+    })
+  ],
+  operation: function(pixels, data) {
+    var pixel = pixels[0];
+    return pixel;
+  },
+  lib: {
+
+  }
+});
+//raster.set('threshold', 0.1);
+
+/**
+ * Map Setup
+ */
+
 var attribution = new ol.Attribution({
   html: 'Tiles © <a href="http://services.arcgisonline.com/ArcGIS/' + 'rest/services/World_Topo_Map/MapServer">ArcGIS</a>'
 });
@@ -10,6 +34,9 @@ var map = new ol.Map({
         attributions: [attribution],
         url: 'https://server.arcgisonline.com/ArcGIS/rest/services/' + 'Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}'
       })
+    }),
+    new ol.layer.Image({
+      source: landcover
     })
   ],
   view: new ol.View({
@@ -22,12 +49,12 @@ var map = new ol.Map({
  * Generate controls
  */
 
-$(function(){
-  landcoverClasses.forEach(function(i){
-    $('#controls').append('<section class="legenditem"> <span class="legendcolor" style="background-color:'+rgbaString(i.color)+'">&nbsp;</span> <span class="legendname">'+i.name+'</span> </section>');  
+$(function() {
+  landcoverClasses.forEach(function(i) {
+    $('#controls').append('<section class="legenditem"> <span class="legendcolor" style="background-color:' + rgbaString(i.color) + '">&nbsp;</span> <span class="legendname">' + i.name + '</span> </section>');
   });
 });
 
-function rgbaString(color){
-  return 'rgba('+ color[0] + ','+ color[1] + ','+ color[2] + ','+ color[3] + ')';
+function rgbaString(color) {
+  return 'rgba(' + color[0] + ',' + color[1] + ',' + color[2] + ',' + color[3] + ')';
 }
