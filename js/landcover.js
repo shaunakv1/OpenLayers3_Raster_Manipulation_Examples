4ce1741d35abd4f77f1d18750e82d4dad9c66d24
shaunakv1 <shaunakv1@gmail.com>
2016-09-16 01:13:46 -0400

    working on color comparison

--- js/landcover.js
+++ js/landcover.js
@@ -1,3 +1,17 @@
+//checkout : https://www.npmjs.com/package/pixelmatch
+//http://zschuessler.github.io/DeltaE/
+//https://github.com/antimatter15/rgb-lab/blob/master/color.js
+
+var modifyPixel = function (pixel){
+  var R=0,G=1,B=2,A=3;
+  var match = [230,38,239,255];
+  /*if(pixel[R] === match [R] && pixel[G] === match [G] && pixel[B] === match [B]){
+
+  }else{
+    pixel[A] = 0;
+  }*/
+  return pixel;
+}
 /**
  * Setup landcover tile cache layer as a raster layer
  */
@@ -10,10 +24,10 @@ var landcover = new ol.source.Raster({
   ],
   operation: function(pixels, data) {
     var pixel = pixels[0];
-    return pixel;
+    return modifyPixel(pixel);
   },
   lib: {
-
+    modifyPixel:modifyPixel
   }
 });
 //raster.set('threshold', 0.1);
