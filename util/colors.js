585b1c306c145ae84fdb4e17e370395000177d44
shaunakv1 <shaunakv1@gmail.com>
2016-09-15 23:36:18 -0400

    created a utility to extract rgba values from arcgis's legend html which is rendered using png image data coming form server

---
+++ util/colors.js
@@ -0,0 +1,19 @@
+var colors = [];
+$(function(){
+	$('tr').each(function(i,ele){
+		var name = $($(ele).find('td')[1]).text().trim() || "Unknown"; 
+		var img = $(ele).find('img')[0];
+		var canvas = document.createElement('canvas');
+		canvas.width = img.width;
+		canvas.height = img.height;
+		canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+		var pixelData = canvas.getContext('2d').getImageData(10, 10, 1, 1).data;
+		//console.log(pixelData, name);
+		colors.push({
+			name: name,
+			color: Array.prototype.slice.call(pixelData)
+		});
+	});	
+	//console.log(colors);
+	console.log(JSON.stringify(colors));
+});
