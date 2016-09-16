68f46fc9511f95564af0f024893ca8f246ca3411
shaunakv1 <shaunakv1@gmail.com>
2016-09-15 23:38:45 -0400

    added landcover_classes.js generated from arcgis legend colors utility script

---
+++ js/landcover_classes.js
@@ -0,0 +1,236 @@
+var landcoverClasses = [
+  {
+    "name": "Background",
+    "color": [
+      0,
+      0,
+      0,
+      0
+    ]
+  },
+  {
+    "name": "Unclassified",
+    "color": [
+      255,
+      255,
+      255,
+      255
+    ]
+  },
+  {
+    "name": "Developed, High Intensity",
+    "color": [
+      242,
+      242,
+      242,
+      255
+    ]
+  },
+  {
+    "name": "Developed, Medium Intensity",
+    "color": [
+      168,
+      154,
+      168,
+      255
+    ]
+  },
+  {
+    "name": "Developed, Low Intensity",
+    "color": [
+      140,
+      117,
+      124,
+      255
+    ]
+  },
+  {
+    "name": "Developed, Open Space",
+    "color": [
+      193,
+      203,
+      69,
+      255
+    ]
+  },
+  {
+    "name": "Cultivated Crops",
+    "color": [
+      84,
+      31,
+      4,
+      255
+    ]
+  },
+  {
+    "name": "Pasture/Hay",
+    "color": [
+      192,
+      159,
+      85,
+      255
+    ]
+  },
+  {
+    "name": "Grassland/Herbaceous",
+    "color": [
+      241,
+      186,
+      139,
+      255
+    ]
+  },
+  {
+    "name": "Deciduous Forest",
+    "color": [
+      38,
+      240,
+      44,
+      255
+    ]
+  },
+  {
+    "name": "Evergreen Forest",
+    "color": [
+      3,
+      57,
+      4,
+      255
+    ]
+  },
+  {
+    "name": "Mixed Forest",
+    "color": [
+      25,
+      159,
+      63,
+      255
+    ]
+  },
+  {
+    "name": "Scrub/Shrub",
+    "color": [
+      107,
+      106,
+      17,
+      255
+    ]
+  },
+  {
+    "name": "Palustrine Forested Wetland",
+    "color": [
+      9,
+      91,
+      90,
+      255
+    ]
+  },
+  {
+    "name": "Palustrine Scrub/Shrub Wetland",
+    "color": [
+      240,
+      107,
+      31,
+      255
+    ]
+  },
+  {
+    "name": "Palustrine Emergent Wetland",
+    "color": [
+      240,
+      38,
+      239,
+      255
+    ]
+  },
+  {
+    "name": "Estuarine Forested Wetland",
+    "color": [
+      60,
+      3,
+      60,
+      255
+    ]
+  },
+  {
+    "name": "Estuarine Scrub/Shrub Wetland",
+    "color": [
+      106,
+      10,
+      106,
+      255
+    ]
+  },
+  {
+    "name": "Estuarine Emergent Wetland",
+    "color": [
+      174,
+      24,
+      174,
+      255
+    ]
+  },
+  {
+    "name": "Unconsolidated Shore",
+    "color": [
+      42,
+      242,
+      241,
+      255
+    ]
+  },
+  {
+    "name": "Bare Land",
+    "color": [
+      242,
+      240,
+      52,
+      255
+    ]
+  },
+  {
+    "name": "Open Water",
+    "color": [
+      2,
+      11,
+      118,
+      255
+    ]
+  },
+  {
+    "name": "Palustrine Aquatic Bed",
+    "color": [
+      10,
+      33,
+      238,
+      255
+    ]
+  },
+  {
+    "name": "Unknown",
+    "color": [
+      103,
+      120,
+      217,
+      255
+    ]
+  },
+  {
+    "name": "Unknown",
+    "color": [
+      248,
+      208,
+      47,
+      255
+    ]
+  },
+  {
+    "name": "Unknown",
+    "color": [
+      172,
+      249,
+      240,
+      255
+    ]
+  }
+]
