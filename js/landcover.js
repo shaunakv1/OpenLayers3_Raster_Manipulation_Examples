f36fea2944d308636cede7b5d2dc7edf175d6294
shaunakv1 <shaunakv1@gmail.com>
2016-09-15 22:31:30 -0400

    switched to using esri dark basemap

--- js/landcover.js
+++ js/landcover.js
@@ -1,12 +1,19 @@
+var attribution = new ol.Attribution({
+  html: 'Tiles © <a href="http://services.arcgisonline.com/ArcGIS/' + 'rest/services/World_Topo_Map/MapServer">ArcGIS</a>'
+});
+
 var map = new ol.Map({
   target: 'map',
   layers: [
     new ol.layer.Tile({
-      source: new ol.source.OSM()
+      source: new ol.source.XYZ({
+        attributions: [attribution],
+        url: 'https://server.arcgisonline.com/ArcGIS/rest/services/' + 'Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}'
+      })
     })
   ],
   view: new ol.View({
-    center: ol.proj.fromLonLat([37.41, 8.82]),
-    zoom: 4
+    center: [-10802627.683927462, 4786251.5431982465],
+    zoom: 5
   })
 });
