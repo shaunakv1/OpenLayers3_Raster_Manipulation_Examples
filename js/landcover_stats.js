a0242510430b73d028ef4bfc6ea4ba1ac9298473
shaunakv1 <shaunakv1@gmail.com>
2016-09-19 00:09:24 -0400

    in process of displaying totals. counts are skewed currently

--- js/landcover_stats.js
+++ js/landcover_stats.js
@@ -2,16 +2,20 @@
 //http://zschuessler.github.io/DeltaE/
 //https://github.com/antimatter15/rgb-lab/blob/master/color.js
 
-var modifyPixel = function(pixel, landcoverClass) {
+var modifyPixel = function(pixel, data) {
     var R = 0,G = 1,B = 2,A = 3;
-    
-    if (landcoverClass) {
-      var matchLab = landcoverClasses[landcoverClass].colorLab;
+    data.total += 1;
+
+    if (data.lc_class) {
+      var matchLab = landcoverClasses[data.lc_class].colorLab;
       var pixelLab = rgb2lab(pixel);
       var deltaEDiff = deltaE(matchLab, pixelLab);
 
       if (deltaEDiff > 3.0) {
         pixel[A] = 0;
+      }
+      else{
+        data.classes[data.lc_class].count += 1;  
       }
     }
 
@@ -30,7 +34,7 @@ var landcover = new ol.source.Raster({
   ],
   operation: function(pixels, data) {
     var pixel = pixels[0];
-    return modifyPixel(pixel, data.lc_class);
+    return modifyPixel(pixel, data);
   },
   lib: {
     landcoverClasses: landcoverClasses,
@@ -40,14 +44,19 @@ var landcover = new ol.source.Raster({
     modifyPixel: modifyPixel
   }
 });
-landcover.set('lc_class', null);
+landcover.set('lc_class', 0);
 
 landcover.on('beforeoperations', function(event) {
   event.data.lc_class = landcover.get('lc_class');
+  event.data.total = 0;
+  event.data["classes"] = {};
+  event.data["classes"][event.data.lc_class]={};
+  event.data["classes"][event.data.lc_class]["count"] = 0;
 });
 
 landcover.on('afteroperations', function(event) {
-  
+  displayStats({total: event.data.classes[event.data.lc_class].count });
+  event.data["classes"][event.data.lc_class]["count"] = 0;
 });
 
 /**
@@ -88,8 +97,16 @@ $(function() {
   $('.legenditem').on('click', function(evt) {
     landcover.set('lc_class', $(this).data('color-id'));
     landcover.changed();
+    
+    displayStats({title: $(this).find('.legendname').text()});
   });
 });
+
+function displayStats(stats){
+  if(stats.title) $('#stats #title').text(stats.title);
+  if(stats.total) $('#stats #total').text(stats.total);
+  if(stats.percent)$('#stats #percent').text(stats.percent);
+}
 
 function rgbaString(color) {
   return 'rgba(' + color[0] + ',' + color[1] + ',' + color[2] + ',' + color[3] + ')';
